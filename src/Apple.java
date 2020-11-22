f7c95907abab1395a3bf36c95d61a3c5ad0757f4
Adbistju <adjytant@gmail.com>
2020-11-22 15:49:38 +0300

    Banana and Apple

---
+++ src/Apple.java
@@ -0,0 +1,6 @@
+public class Apple extends Fruit{
+    static int index=0;
+    public Apple() {
+        super(4, index++);
+    }
+}
---
+++ src/Banana.java
@@ -0,0 +1,6 @@
+public class Banana extends Fruit{
+    static int index=0;
+    public Banana() {
+        super(3, index++);
+    }
+}
